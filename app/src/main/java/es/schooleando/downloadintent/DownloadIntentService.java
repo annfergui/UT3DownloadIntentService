7ff56219ed7339b9948086b000a3173c32b908b3
Anna <annfergui@gmail.com>
2017-05-22 10:48:39 +0200

    Moificado

--- app/src/main/java/es/schooleando/downloadintent/DownloadIntentService.java
+++ app/src/main/java/es/schooleando/downloadintent/DownloadIntentService.java
@@ -1,8 +1,25 @@
 package es.schooleando.downloadintent;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+import android.os.SystemClock;
 import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 /**
@@ -19,17 +36,149 @@ public class DownloadIntentService extends IntentService {
 
 
     private static final String TAG = "DownloadIntentService";
+    private int codRespuesta;
+    private  int tamRecurso;
+    private String tipo;
+    private Bitmap imgBmPDownload;
+
 
     public DownloadIntentService() {
         super("DownloadIntentService");
+
+
+
+
+
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
 
+
+        ResultReceiver resultado;
         // Ejemplo de como logear
         Log.d(TAG, "Servicio arrancado!");
+if(intent!=null) {
+    //Inicializamos el Resultreceiver con el intent recibido
+    resultado = intent.getParcelableExtra("receiverTag");
 
+    //Obtenemos el URL del Intent
+    String urlString = intent.getStringExtra("url");
+    Log.d(TAG, "El URL del intent es: " + urlString);
+
+    Bundle b = new Bundle();
+
+    //creamos el objeto ConnectivityManager
+    ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo ni = cm.getActiveNetworkInfo();
+    if (ni != null && ni.isConnected()) {
+        // Aquí deberás descargar el archivo y notificar a la Activity mediante un ResultReceiver que recibirás en el intent.
+    URL url=null;
+        try {
+            url = new URL(urlString);
+            HttpURLConnection conex = (HttpURLConnection) url.openConnection();
+            //peticion de descarga y conexión
+            conex.setRequestMethod("HEAD");
+            conex.connect();
+
+            //aquí vamos a obtener los datos de la descarga
+            codRespuesta = conex.getResponseCode();
+            tipo = conex.getContentType();
+            tamRecurso = conex.getContentLength();
+
+            //comprobamos si nos ha devuelto un codigo ok
+            if (codRespuesta == 200) {
+                if (tipo.startsWith("image/")) {
+                    InputStream is = url.openStream();
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+                    byte[] buffer = new byte[1024];
+                    int i;
+                    int tot = 0;
+
+                    //ponemos el log de descargando
+
+                    Log.d(TAG, "La descarga esta en curso...");
+
+                    while ((i = is.read(buffer)) != -1) {
+                        bos.write(buffer, 0, i);
+
+                        if (tamRecurso>0) {
+
+
+                            b.putInt("DownloadService",bos.size() * 100 /tamRecurso);
+
+                        } else {
+                            b.putInt("DownloadService",i*-1);
+
+                        }
+                        resultado.send(this.PROGRESS, b);
+                    }
+                    String[] datos = urlString.split("/");
+                    String[] trozo = datos[datos.length-1].split("\\.");
+
+                    if (trozo.length<2) {
+                        trozo = new String[]{"unknown", "jpg"};
+                    }
+
+                    //Archivo temporal
+                    File outputDir = getExternalCacheDir();
+
+                    File outputFile = File.createTempFile(trozo[0], "." + trozo[1], outputDir);
+                    outputFile.deleteOnExit();      //Eliminar archivo temporal al SALIR
+
+                    FileOutputStream fos = new FileOutputStream(outputFile);
+                    fos.write(bos.toByteArray());
+
+                    b.putString("urlString", outputFile.getPath());
+                    resultado.send(FINSISHED, b);
+
+
+                    //cerraremos los flujos de entrada/salida
+
+                    bos.close();
+                    is.close();
+
+                    //byte[]arrayImagenes=bos.toByteArray();
+                    //imgBmPDownload= BitmapFactory.decodeByteArray(arrayImagenes,0,arrayImagenes.length);
+
+                    Log.d(TAG, "Descarga completada");
+                    resultado.send(FINSISHED, b);
+                    b.putString("DownloadService", "Finalizada la descarga");
+                    b.putParcelable("bitmapImagen",imgBmPDownload);
+
+                } else {
+                    Log.d(TAG, "La URL no pertenece a ninguna imagen");
+                    b.putString("DownloadService", "Error:La URL no corresponde a una imagen");
+                    resultado.send(ERROR, b);
+                }
+            } else {
+                Log.d(TAG, "Error al conectar:" + codRespuesta);
+                b.putString("DownloadService", "Error al conectar");
+                resultado.send(ERROR, b);
+            }
+
+
+        } catch (MalformedURLException e) {
+            b = new Bundle();
+            b.putString("DownloadService", "URL incorrecta");
+            resultado.send(this.ERROR, b);
+            e.printStackTrace();
+        } catch (IOException e) {
+            b = new Bundle();
+            b.putString("DownloadService", "Error de descarga");
+            resultado.send(this.ERROR, b);
+            e.printStackTrace();
+        }
+//aqui hacemos el else de la conexión
+    } else {
+
+        b.putString("DownloadService", "Error de conexión");
+        resultado.send(ERROR, b);
+    }
+}else {
+  Log.d(TAG,"Error en el DownloadService");
+}
         // Aquí deberás descargar el archivo y notificar a la Activity mediante un ResultReceiver que recibirás en el intent.
 
         // Deberamos obtener el ResultReceiver del intent
--- app/src/main/java/es/schooleando/downloadintent/MainActivity.java
+++ app/src/main/java/es/schooleando/downloadintent/MainActivity.java
@@ -1,14 +1,39 @@
 package es.schooleando.downloadintent;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar pb;
+    private TextView tv;
+    private EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //recogemos las variables del layout
+        pb = (ProgressBar) findViewById(R.id.progress);
+        pb.setVisibility(View.INVISIBLE);
+        tv = (TextView) findViewById(R.id.estadoTV);
+        et = (EditText) findViewById(R.id.urlET);
 
         // Añade en el interfaz un botón y un TextView, como mínimo.
 
@@ -21,4 +46,75 @@ public class MainActivity extends AppCompatActivity {
 
         // después deberás llamar al servicio con el intent.
     }
-}
+       public void descargURL(View v){
+        if (!et.getText().toString().trim().equals("")) {
+            pb.setVisibility(View.VISIBLE);
+
+            Intent i = new Intent(this, DownloadIntentService.class);
+            //llamamos al url para obtener la cadena introducida
+
+            i.putExtra("url", et.getText().toString());
+
+            ResultReceiver receiver = new ResultReceiver(new Handler()) {
+                protected void onReceiveResult(int resultCode, Bundle resultData) {
+                    //vamos a recibir los resultados y los pasaremos a un switch-case
+                    switch (resultCode) {
+                        case DownloadIntentService.PROGRESS:
+                            int progreso = resultData.getInt("DownloadService");
+                            pb.setIndeterminate(progreso < 0);
+                            if (progreso > 0) {
+                                tv.setText("" + progreso + "%");
+                            }
+                            pb.setProgress(progreso);
+                            break;
+                        case DownloadIntentService.FINSISHED:
+                            String urlString = resultData.getString("urlString");
+                            pb.setVisibility(View.INVISIBLE);
+                            tv.setText("");
+                            File fichero = new File(urlString);
+                            if (fichero.exists()) {
+                                MimeTypeMap mime = MimeTypeMap.getSingleton();
+                                String ext = android.webkit.MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(fichero).toString());
+                                String tipo = mime.getMimeTypeFromExtension(ext);
+                                try {
+
+                                    Intent intent = new Intent();
+                                    intent.setAction(Intent.ACTION_VIEW);
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                        Uri contentUri = FileProvider.getUriForFile(MainActivity.this, "com.your.package.fileProvider", fichero);
+                                        intent.setDataAndType(contentUri, tipo);
+                                    } else {
+                                        intent.setDataAndType(Uri.fromFile(fichero), tipo);
+                                    }
+                                    startActivityForResult(intent, 0);
+                                }catch (ActivityNotFoundException e) {
+                                    Toast.makeText(MainActivity.this, "No se encuentra la Activity", Toast.LENGTH_LONG).show();
+                                }
+
+                            }else{
+                                Toast.makeText(MainActivity.this,"No existe el fichero",Toast.LENGTH_LONG).show();
+
+
+                    }
+                            break;
+
+                        case DownloadIntentService.ERROR:
+                            String mensaje=resultData.getString("DownloadService");
+                            pb.setVisibility(View.INVISIBLE);
+                            tv.setText("");
+                            Toast.makeText(MainActivity.this,mensaje,Toast.LENGTH_LONG).show();
+                            break;
+
+                }
+
+            }
+        };
+        i.putExtra("receiverTag",receiver);
+            startService(i);
+        }else{
+            Toast.makeText(MainActivity.this,"La URL no ha sido introducida",Toast.LENGTH_SHORT).show();
+        }
+    }
+    }
+
